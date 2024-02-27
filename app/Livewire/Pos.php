a673f2fe42407f497f4a49b665f41fcdac215de7
BLKK Nurul Huda <belajarkoding98@gamil.com>
2024-02-27 21:00:17 +0700

    Master Aplikasi Kasir El-Huda Sederhana

---
+++ app/Livewire/Pos.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\OrderProduct;
+
+class Pos extends Component
+{
+    public $search = '';
+    public $product;
+    public $order;
+    public $total_price;
+    public $paid_amount;
+
+    public function render()
+    {
+        $this->order = Order::where('done_at', null)
+            ->with('orderProducts')
+            ->latest()
+            ->first();
+        $this->total_price = $this->order->total_price ?? 0;
+        return view('livewire.pos', [
+            'products' => Product::search($this->search)->paginate(12),
+            'order' => $this->order
+        ]);
+    }
+
+    public function createOrder()
+    {
+        $this->order = Order::where('done_at', null)
+            ->latest()
+            ->first();
+
+        if ($this->order ==  null) {
+            $this->order = Order::create([
+                'invoice_number' => $this->generateUniqueCode()
+            ]);
+        }
+        session()->flash('message', 'Sukses mulai transaksi, silakan pilih produk.');
+    }
+
+    public function updateCart($productId, $isAdded = true)
+    {
+        try {
+            if ($this->order) {
+                $product = Product::findOrFail($productId);
+                $orderProduct = OrderProduct::where('order_id', $this->order->id)
+                    ->where('product_id', $productId)
+                    ->first();
+
+                if ($orderProduct) {
+                    if ($isAdded) {
+                        $orderProduct->increment('quantity', 1);
+                        $stock = ((int)$product->stock - 1);
+                    } else {
+                        $orderProduct->decrement('quantity', 1);
+                        $stock = ((int)$product->stock + 1);
+                        if ($orderProduct->quantity < 1) {
+                            $stock = ((int)$product->stock + 2);
+                            $orderProduct->delete();
+                            session()->flash('message', 'Produk berhasil dihapus dari keranjang');
+                            return;
+                        }
+                    }
+                    // $qty = stock;
+                    // dd($qty);
+                    $orderProduct->save();
+                } else {
+                    if ($isAdded) {
+                        OrderProduct::create([
+                            'order_id' => $this->order->id,
+                            'product_id' => $product->id,
+                            'unit_price' => $product->selling_price,
+                            'quantity' => 1
+                        ]);
+                        $stock = (int)$product->stock - 1;
+                    } else {
+                        $stock = (int)$product->stock + 1;
+                    }
+                }
+                //kurangi stok dari produk
+                // dd($stock);
+                Product::where(
+                    'id',
+                    $product->id
+                )->update(['stock' => $stock]);
+
+                $this->total_price = $this->order->total_price ?? 0;
+
+                session()->flash('message', $isAdded ? 'Produk berhasil ditambahkan' : 'Produk berhasil dihapus dari keranjang');
+            } else {
+                session()->flash('message', 'Klik Mulai Transaksi Dahulu');
+            }
+        } catch (ValidationException $e) {
+            dd($e);
+        } catch (\Exception $e) {
+            dd($e);
+        }
+    }
+
+    public function done()
+    {
+        $this->validate([
+            'paid_amount' => 'required'
+        ]);
+
+        $this->order->update([
+            'paid_amount' => $this->paid_amount,
+            'done_at' => now()
+        ]);
+
+        session()->flash('message', 'Order/Transaksi selesai');
+        return redirect()->route('order');
+    }
+
+    function generateUniqueCode($length = 6)
+    {
+        $number = uniqid();
+        $varray = str_split($number);
+        $len = sizeof($varray);
+        $uniq = array_slice($varray, $len - 6, $len);
+        $uniq = implode(",", $uniq);
+        $uniq = str_replace(',', '', $uniq);
+
+        return $uniq;
+    }
+}
